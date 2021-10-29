e3ff6e01e6ebb64d12216d4eefc0eeeb29db82af
Raunak Poonia <pooniaraunak@gmail.com>
2021-10-29 12:48:22 +0530

    created weather detail page

---
+++ src/app/weather-detail/weather-detail.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WeatherDetailComponent } from './weather-detail.component';
+
+describe('WeatherDetailComponent', () => {
+  let component: WeatherDetailComponent;
+  let fixture: ComponentFixture<WeatherDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WeatherDetailComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WeatherDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/weather-detail/weather-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-detail',
+  templateUrl: './weather-detail.component.html',
+  styleUrls: ['./weather-detail.component.css']
+})
+export class WeatherDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
