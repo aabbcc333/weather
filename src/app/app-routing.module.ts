99e18bbfaeb56545f489c90747856bfde30b0824
Raunak Poonia <pooniaraunak@gmail.com>
2021-10-29 13:02:02 +0530

    added country list page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CountryListComponent } from './country-list/country-list.component';
+import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CountryListComponent },
+  { path: 'weatherDetail', component: WeatherDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
+import { CountryListComponent } from './country-list/country-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherDetailComponent
+    WeatherDetailComponent,
+    CountryListComponent
   ],
   imports: [
     BrowserModule,
